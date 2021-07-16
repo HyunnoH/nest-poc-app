8bbc7eda99745b0a4851a0e12ec59d558e0462ee
Hyun Oh <hoh@mondrian.ai>
2021-07-16 10:49:29 +0900

    Remove import statements

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
-import { NoticeModule } from './notice/notice.module';
-import { BoardModule } from './board/board.module';
+
 import { UsersModule } from './users/users.module';
 import { BoardsModule } from './boards/boards.module';
 import { NoticesModule } from './notices/notices.module';
@@ -11,6 +9,6 @@ import { NoticesModule } from './notices/notices.module';
 @Module({
   controllers: [AppController],
   providers: [AppService],
-  imports: [UserModule, NoticeModule, BoardModule, UsersModule, BoardsModule, NoticesModule],
+  imports: [UsersModule, BoardsModule, NoticesModule],
 })
 export class AppModule {}
