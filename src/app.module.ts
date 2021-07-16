7b4aeb46f4bb5d460e01c78598bc7f0b2c97c472
Hyun Oh <hoh@mondrian.ai>
2021-07-16 10:33:36 +0900

    Add basic resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { NoticeModule } from './notice/notice.module';
+import { BoardModule } from './board/board.module';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
+  imports: [UserModule, NoticeModule, BoardModule],
 })
 export class AppModule {}
